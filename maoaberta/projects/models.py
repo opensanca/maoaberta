a41d8313d8c32d8c99ca69cadceddcbb83b76fc2
Lucas Matheus Testa <lucasmatheus3492@gmail.com>
2016-07-19 23:47:45 -0300

    created project model

--- maoaberta/projects/models.py
+++ maoaberta/projects/models.py
@@ -1,1 +1,13 @@
-# end
+from django.db import models
+
+
+class Project(models.Model):
+    """
+    Each project is an activity and/or event organized by an organization.
+    """
+    name = models.CharField(max_length=30)
+    date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return '{}'.format(self.nome)
