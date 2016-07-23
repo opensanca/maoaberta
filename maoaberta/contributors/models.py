d7d038de85f76a8e3e41a2f2b9b01b117236a7ea
Luiz Menezes <luiz.menezesf@gmail.com>
2016-07-23 11:14:57 -0300

    add contributor and organization models

--- maoaberta/contributors/models.py
+++ maoaberta/contributors/models.py
@@ -1,1 +1,27 @@
-# end
+from django import models
+from django.contrib.auth import get_user_model
+from django.contrib.translations import ugettext_lazy as _
+
+from organizations.models import Organization
+from projects.models import Cause, Project
+
+
+User = get_user_model()
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(User, verbose_name=_('Login user'))
+
+    photo = models.ImageField(path='contributor_photos', verbose_name=_('Contributor photo'))
+
+    supported_causes = models.ManyToManyField(Cause)
+
+    supported_projects = models.ManyToManyField(Project)
+
+    supported_organizations = models.ManyToManyField(Organization)
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
--- maoaberta/organizations/models.py
+++ maoaberta/organizations/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from contributors.models import Contributor
+from projects.models import Project
+
 
 class Necessity(models.Model):
     """
@@ -8,6 +11,58 @@ class Necessity(models.Model):
     """
     name = models.CharField(verbose_name=_('Name'), max_length=20)
     satisfied = models.BooleanField(verbose_name=_('Satisfied'), default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(
+        max_length=64, verbose_name=_('Name'), help_text=_('Organization name')
+    )
+
+    description = models.TextField(
+        verbose_name=_('Description'), help_text=_('Organization description')
+    )
+
+    photo = models.ImageField(verbose_name=_('Photo'), path='organization_photos')
+
+    coordinator = models.ForeignKey(
+        Contributor, verbose_name=_('Coordinator'),
+        help_text=_('Person responsible for the organization')
+    )
+
+    projects = models.ManyToManyField(Project)
+
+    necessities = models.ManyToManyField(Necessity)
+
+    necessity_description = models.TextField(
+        verbose_name=_('Necessity description'),
+        help_text=_('Text to explain the organization material needs')
+    )
+
+    email = models.EmailField(
+        verbose_name=_('Organization email'), blank=True,
+        help_text=_('Contact email for the organization')
+    )
+
+    homepage_url = models.URLField(
+        verbose_name=_('Homepage URL'), blank=True,
+        help_text=_('Organization homepage link'),
+    )
+
+    facebook_url = models.URLField(
+        verbose_name=_('Facebook URL'), blank=True,
+        help_text=_('Organization facebook link')
+    )
+
+    twitter_url = models.URLField(
+        verbose_name=_('Twitter URL'), blank=True,
+        help_text=_('Organization twitter link')
+    )
+
+    def __repr__(self):
+        return '<Organization({})>'.format(self.name)
 
     def __str__(self):
         return self.name
