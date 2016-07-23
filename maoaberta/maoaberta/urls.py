874e42098374baf4b12321461669094a5b02410a
Luiz Menezes <luiz.menezesf@gmail.com>
2016-07-23 12:02:20 -0300

    add base template

--- maoaberta/maoaberta/urls.py
+++ maoaberta/maoaberta/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from organizations.views import HomePageView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home'),
 ]
