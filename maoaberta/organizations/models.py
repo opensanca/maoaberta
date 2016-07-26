399d10b59e8759a2f1e9ff97eae3fb4d001c784f
Lucas Matheus Testa <lucasmatheus3492@gmail.com>
2016-07-26 19:15:45 -0300

    changes in project model and necessity model

--- maoaberta/organizations/models.py
+++ maoaberta/organizations/models.py
@@ -6,7 +6,7 @@ class Necessity(models.Model):
     Item or service that an organization regularly needs
     """
     name = models.CharField(max_length=20)
-    interval = models.FloatField()
+    satisfied = models.BooleanField(default=False)
 
     def __str__(self):
         return '{}'.format(self.name)
--- maoaberta/projects/models.py
+++ maoaberta/projects/models.py
@@ -1,13 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+STATUS = (
+    ('open', 'open'),
+    ('running', 'running'),
+    ('closed', 'closed')
+)
 
 
 class Project(models.Model):
     """
     Each project is an activity and/or event organized by an organization.
     """
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     date = models.DateField()
     description = models.TextField()
+    status = models.CharField(choices=STATUS, max_length=7, default='open')
+    responsible = models.ForeignKey(User, related_name='projects')
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return '{}'.format(self.title)
