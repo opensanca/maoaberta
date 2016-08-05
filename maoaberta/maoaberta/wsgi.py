5cc487626afef093f857efc17173fdc953bea775
Luiz Menezes <luiz.menezesf@gmail.com>
2016-08-05 05:50:59 -0300

    serve static files using dj-static

--- maoaberta/maoaberta/wsgi.py
+++ maoaberta/maoaberta/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling, MediaCling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maoaberta.settings")
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
