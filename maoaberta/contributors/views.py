4af2fc11cfc2df07a61a1dfa6bb794409c2b1e1b
Leticia Rostirola <let.rostirola@gmail.com>
2016-10-29 15:47:52 -0200

    update contributor-details create detail view

--- maoaberta/contributors/views.py
+++ maoaberta/contributors/views.py
@@ -1,11 +1,12 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import DetailView, TemplateView
+from .models import Contributor
 
 class LoginView(TemplateView):
 
     template_name = 'contributors/login.html'
 
 
-class ContributorView(TemplateView):
+class ContributorDetailView(DetailView):
 
     template_name = 'contributors/contributor_details.html'
+    model = Contributor
