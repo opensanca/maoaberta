6e37faee88a04f23c93fa979a9d51f0a43208c2a
Leticia Rostirola <let.rostirola@gmail.com>
2016-10-30 15:49:49 -0200

    add contributors test data Create test data using factory_boy

---
+++ maoaberta/contributors/tests/factories.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+
+import factory
+from contributors.models import Contributor
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = {"username", }
+
+    username = factory.Sequence(lambda n: 'george%s' % n)
+    first_name = factory.Faker("first_name", locale="pt_BR")
+    last_name = factory.Faker("last_name", locale="pt_BR")
+    email = factory.Faker("email", locale="pt_BR")
+    password = "senhamuitoforte"
+
+
+class ContributorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Contributor
+
+    user = factory.RelatedFactory(UserFactory)
--- maoaberta/maoaberta/settings.py
+++ maoaberta/maoaberta/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'contributors',
     'organizations',
     'projects'
